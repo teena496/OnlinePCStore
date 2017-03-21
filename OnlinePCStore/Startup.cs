162d65d8c66eb5ef9f4c33947123f6a2c3915a2a
Teena Thomas <teena496@outlook.com>
2017-03-21 22:46:59 +0530

    Add project files.

---
+++ OnlinePCStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlinePCStore.Startup))]
+namespace OnlinePCStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
